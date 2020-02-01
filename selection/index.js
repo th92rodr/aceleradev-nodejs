9f2e3a6aa4ce02c0c3999e0ed5ec94a4ee43641f
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-02-01 11:42:00 -0300

    format selection index file

--- selection/index.js
+++ selection/index.js
@@ -7,56 +7,61 @@ const sha1 = require('sha1');
 const token = '';
 
 async function getJSON() {
-    const response = await fetch(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`);
-    const data = await response.json();
-    //console.log(data);
+  const response = await fetch(
+    `https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`
+  );
+  const data = await response.json();
+  //console.log(data);
 
-    data.decifrado = decipher(data.cifrado, data.numero_casas);
-    data.resumo_criptografico = sha1(data.decifrado);
-    //console.log(data);
+  data.decifrado = decipher(data.cifrado, data.numero_casas);
+  data.resumo_criptografico = sha1(data.decifrado);
+  //console.log(data);
 
-    fs.writeFileSync('answer.json', JSON.stringify(data, null, 2));
+  fs.writeFileSync('answer.json', JSON.stringify(data, null, 2));
 
-    /*
+  /*
     await fs.readFile('answer.json', (err, data) => {
         if (err) throw err;
         console.log(JSON.parse(data));
     });
     */
 
-    const form = new FormData();
-    form.append('answer', fs.createReadStream('answer.json'), {
-        filename: 'answer.json',
-        contentType: 'application/json',
-    });
+  const form = new FormData();
+  form.append('answer', fs.createReadStream('answer.json'), {
+    filename: 'answer.json',
+    contentType: 'application/json'
+  });
 
-    try {
-        const res = await axios.post(`https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=${token}`, form, {
-            headers: form.getHeaders()
-        });
-        console.log(res.status);
-    } catch (error) {
-        console.log('error: ', error);
-    }
-
+  try {
+    const res = await axios.post(
+      `https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=${token}`,
+      form,
+      {
+        headers: form.getHeaders()
+      }
+    );
+    console.log(res.status);
+  } catch (error) {
+    console.log('error: ', error);
+  }
 }
 
 function decipher(str, n) {
-  var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
   str = str.toLowerCase();
 
-  let str2 = "";
+  let str2 = '';
   for (let i = 0; i < str.length; i++) {
-
-    if (str[i] == " " || str[i].match(/^[-&\/\\#,+()$~%.'":*\!\?\<>_{}0-9]+$/i) ) {
+    if (
+      str[i] == ' ' ||
+      str[i].match(/^[-&\/\\#,+()$~%.'":*\!\?\<>_{}0-9]+$/i)
+    ) {
       str2 = str2 + str[i];
-
     } else {
       var normal_index = alphabet.indexOf(str[i]);
       if (normal_index - n < 0) {
         var index = alphabet.length - (n - normal_index);
-
       } else {
         var index = normal_index - n;
       }
