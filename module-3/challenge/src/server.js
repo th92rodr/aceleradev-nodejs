53be2e42e8005d841837aad85bfe4b02678aa889
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-03-10 21:57:15 -0300

    update module 3 challenge

--- module-3/challenge/src/server.js
+++ module-3/challenge/src/server.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const imdb_movies = require('../imdb-movies');
+
 const app = express();
 
 function getRandomNumber(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-app.get('/v1/movie', async (req, res, next) => {
-  const rand_id = getRandomNumber(imdb_movies.length);
-  const obj = imdb_movies.find((movie) => movie.Rank === rand_id);
-  res.status(200).json({ director: obj.Director, movie: obj.Title });
+app.get('/v1/movie', async (req, res) => {
+  const randomID = getRandomNumber(imdb_movies.length);
+  const movie = imdb_movies.find((movie) => movie.Rank === randomID);
+  res.status(200).json({ director: movie.Director, movie: movie.Title });
 });
 
-app.get('/v1/movie/:director', async (req, res, next) => {
+app.get('/v1/movie/:director', async (req, res) => {
   const { director } = req.params;
-  const arr = imdb_movies.filter((movie) => movie.Director == director);
-  const rand_id = getRandomNumber(arr.length);
-  const obj = arr[rand_id];
-  res.status(200).json({ director: obj.Director, movie: obj.Title });
+  const directorMovies = imdb_movies.filter(
+    (movie) => movie.Director == director
+  );
+  const randomID = getRandomNumber(directorMovies.length);
+  const movie = directorMovies[randomID];
+  res.status(200).json({ director: movie.Director, movie: movie.Title });
 });
 
 const start = async (port = 8080) => {
